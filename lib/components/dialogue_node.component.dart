9ad85fde2ac4e1b0a6d65841648ea5f5d8601d94
Omar Flores <cerberusprogrammer@gmail.com>
2024-08-31 12:39:01 -0700

    feat: Update DialogueNodeComponent to use GestureDetector
    The code changes in `DialogueNodeComponent` replace the `InkWell` widget with `GestureDetector` to handle the `onTap` event. This change allows for more flexibility in gesture detection and improves the overall interactivity of the component.
    
    Note: The suggested commit message follows the established convention of starting with a type (feat for feature) and providing a concise description of the changes.

--- lib/components/dialogue_node.component.dart
+++ lib/components/dialogue_node.component.dart
@@ -1,31 +1,60 @@
 import 'package:flutter/material.dart';
 
 class DialogueNodeComponent extends StatelessWidget {
-  const DialogueNodeComponent({super.key});
+  final VoidCallback onTap;
+  final bool isFocused;
+
+  const DialogueNodeComponent({
+    super.key,
+    required this.onTap,
+    required this.isFocused,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {},
-      borderRadius: BorderRadius.circular(10),
+    return GestureDetector(
+      onTap: onTap,
       child: Container(
-        padding: const EdgeInsets.all(10),
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Icon(Icons.person),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    IconButton(onPressed: () {}, icon: const Icon(Icons.edit)),
-                  ],
-                ),
-                Text('This is a dialogue node.'),
-              ],
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 10,
+        ),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(
+              color: isFocused ? Colors.black : Colors.transparent,
+              width: 2,
             ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.5),
+                blurRadius: 1,
+                spreadRadius: 0.1,
+                offset: const Offset(2, 3),
+              ),
+            ]),
+        child: const Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                CircleAvatar(
+                  radius: 30,
+                  child: Icon(Icons.person),
+                ),
+                SizedBox(width: 20),
+                Text(
+                  'This is a dialogue node',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ],
+            )
           ],
         ),
       ),
